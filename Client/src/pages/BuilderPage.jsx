6a9aa1995d7708457b6d2429dd141f857bb6a13f
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-04 21:02:39 +0530

    Refactor BuilderPage component; implement project loading logic and state management with useEffect

--- Client/src/pages/BuilderPage.jsx
+++ Client/src/pages/BuilderPage.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { useNavigate, useParams } from 'react-router-dom';
 
 const BuilderPage = () => {
+
+  const {id} = useParams()
+  const navigate = useNavigate()
+  const [leftTab, setLeftTab] = useState("chat");
+  const [publishing, setPublishing] = useState(false);
+  const [publishUrl, setPublishUrl] = useState(null);
+
+ const {activeProject, loadingActiveProject, activeFile , showCode, setActiveFile, setShowCode, loadProject, logout} = useAppContext();
+
+ useEffect(()=>{
+    if(!id) return;
+ },[id, loadProject])
+
+ useEffect(()=>{
+    if(!id || !activeProject) return;
+    if(activeProject.status === "pending" || activeProject.status === "generating"){
+      const interval = setInterval(()=>{
+        loadProject(id, true)
+      },1500)
+      return ()=> clearInterval(interval)
+    }
+ },[id, loadProject , activeProject])
+
+ if(loadingActiveProject || !activeProject){
+  return <Loading />
+ }
+
   return (
-    <div>
-      
-    </div>
+    <div>BuilderPage</div>
   )
 }
 
